68a62a9c12e753b0459836a1a5cef010cdb0060b
Henry <henryajacobson@gmail.com>
2020-03-24 14:30:54 -0400

    Finished load test, added push test

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 #include "execute.h"
 #include "memory.h"
 #include "cpu.h"
+#include "assert.h"
 
 // Writes the given instrs into memory.
 void fillMemory(int n, uint8_t* instrs) {
@@ -11,15 +12,30 @@ void fillMemory(int n, uint8_t* instrs) {
   }
 }
 
-void test1() {
-  uint8_t instrs[] = {0x06, 0xFF, 0x10};
-  fillMemory(3, instrs);
+// LD B, 0xFF
+// LD A, B
+void testLD() {
+  uint8_t instrs[] = {0x06, 0xFF, 0x78, 0x10};
+  fillMemory(4, instrs);
   CPU();
-  printf("B = %uh\n", B());
+  assert(A() == 0xFF);
+  printf("PASSED testLD\n");
+}
+
+// LD BC, 0xABCD
+// PUSH BC
+// POP DE
+void testPUSH() {
+  uint8_t instrs[] = {0x01, 0xCD, 0xAB, 0xC5, 0xD1, 0x10};
+  fillMemory(6, instrs);
+  CPU();
+  assert(DE() == 0xABCD);
+  printf("PASSED testPUSH\n");
 }
 
 int main() {
   memInit();
-  test1();
+  testLD();
+  testPUSH();
   return 0;
 }
